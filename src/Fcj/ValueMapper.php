0d9677a9d62dbdbc2e8ea40545e98f05c6c9fb6d
Fabien Cadet <cadet@pc126.cines.fr>
2014-03-14 19:15:01 +0100

    ValueMapper: map2(..., $magics=false) & getSettableProperties(..., $magics=false) : ignoring php class magics __set & __call presence.

--- src/Fcj/ValueMapper.php
+++ src/Fcj/ValueMapper.php
@@ -69,14 +69,15 @@ class ValueMapper
      * @param mixed $source Typically an object or array, possibly a \Traversable (not tested).
      * @param object|array $target
      * @param array $ppaths An eventually empty list of property paths pairs (key=>value).
+     * @param bool $magics Whether or not to consider the presence of PHP class magics __set / __call as implicit setters, defaults to false.
      * @return mixed $target gets returned.
      */
-    public static function map2($source, $target = array(), array $ppaths = array())
+    public static function map2($source, $target = array(), array $ppaths = array(), $magics=false)
     {
         $accessor = PropertyAccess::createPropertyAccessor();
         // If no property path mappings list are provided, infer it :
         if (! $ppaths) {
-            $s = self::getSettableProperties($source);
+            $s = self::getSettableProperties($source, $magics);
             // If $target is an empty array, a map of source key to null, as
             // these get infered as '[key]' ppath later in the below loop :
             if (is_array($target) && !$target)
@@ -84,7 +85,7 @@ class ValueMapper
             // Else the property paths list is the common settable properties
             // from $source & $target :
             else {
-                $t = self::getSettableProperties($target);
+                $t = self::getSettableProperties($target, $magics);
                 $u = array_intersect($s, $t);
                 $ppaths = array_combine($u, $u);
             }
@@ -105,9 +106,10 @@ class ValueMapper
      * TODO: See if a \Traversable thing would work too.
      *
      * @param array|object $source
+     * @param bool $magics If true, PHP class magics __set / __call on $source entail that properties do not need settters, default to false.
      * @return array list<string> of property names (or array keys).
      */
-    public static function getSettableProperties($source)
+    public static function getSettableProperties($source, $magics=false)
     {
         $props = array();
         if (is_array($source)) {
@@ -120,7 +122,7 @@ class ValueMapper
                 return $p->getName();
             }, $reflC->getProperties());
             // Filter out properties that do not have setX() :
-            if (!$reflC->hasMethod('__set') && !$reflC->hasMethod('__call')) {
+            if (!$magics || (!$reflC->hasMethod('__set') && !$reflC->hasMethod('__call'))) {
                 $props = array_filter($props, function($p) use ($reflC) {
                     $setter = 'set' . Inflector::camelize($p);
                     return $reflC->hasMethod($setter);
